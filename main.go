c83aa0b87fa3d4891561e3c3a43da18a0f755f81
Vladimir Kanubrikov <vovank86@gmail.com>
2020-06-05 14:28:39 +0300

    fix

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for i, val := range slice {
+	for _, val := range slice {
 		if val%2 != 0 {
-			fmt.Println(i, "Odd")
+			fmt.Println(val, "Odd")
 		} else {
-			fmt.Println(i, "Even")
+			fmt.Println(val, "Even")
 		}
 	}
 }
